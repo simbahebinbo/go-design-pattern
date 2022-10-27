fc00a5bf4a0e24dd3fb839f0371b8d608e5a8bfb
simbahebinbo <lansheng228@protonmail.com>
2022-10-27 18:28:32 +0800

    编译通过

--- memento/memento.go
+++ memento/memento.go
@@ -1,4 +1,4 @@
-//备忘录模式
+// 备忘录模式
 package memento
 
 import (
@@ -8,40 +8,40 @@ import (
 )
 
 //意图：保存程序内部状态到外部，又不希望暴露内部状态的情形,可以离线保存内部状态，如保存到数据库，文件等
-//解决:捕获一个对象的内部状态，并在该对象之外保存这个状态，这样可以在以后将对象恢复到原先保存的状态。
+//解决: 捕获一个对象的内部状态，并在该对象之外保存这个状态，这样可以在以后将对象恢复到原先保存的状态。
 //使用场景： 游戏存副本等
 
 //实例:游戏状态恢复
 
-//游戏备份
+// 游戏备份
 type Memento interface {
 	Save(*Game)  //存档
 	Load() *Game //载入
 }
 
-//游戏
+// 游戏
 type Game struct {
 	Hp int
 	Mp int
 }
 
-//初始化游戏
+// 初始化游戏
 func GameInit() *Game {
 	return &Game{}
 }
 
-//玩游戏
+// 玩游戏
 func (g *Game) Play(mpDelta, hpDelta int) {
 	g.Mp += mpDelta
 	g.Hp += hpDelta
 }
 
-//游戏状态
+// 游戏状态
 func (g *Game) Status() {
 	fmt.Printf("Current HP:%d, MP:%d\n", g.Hp, g.Mp)
 }
 
-//游戏备份
+// 游戏备份
 type GameMemento struct {
 }
 
--- memento/memento_test.go
+++ memento/memento_test.go
@@ -1,30 +1,36 @@
 package memento
 
-func ExampleGame_Play() {
+import "testing"
+
+func TestExampleGame_Play(t *testing.T) {
 	//初始化游戏
-	g := GameInit()
+	g1 := GameInit()
 	//查看游戏状态
-	g.Status()
+	g1.Status()
 	//玩游戏
-	g.Play(5, 5)
+	g1.Play(5, 5)
 	//查看游戏状态
-	g.Status()
+	g1.Status()
 	//存档
 	gm := &GameMemento{}
-	gm.Save(g)
+	gm.Save(g1)
 	//初始化游戏
-	g = GameInit()
+	g2 := GameInit()
 	//查看游戏状态
-	g.Status()
+	g2.Status()
 	//恢复
-	g = gm.Load()
+	g2 = gm.Load()
 	//查看游戏状态
-	g.Status()
+	g2.Status()
+	//恢复
+	g3 := gm.Load()
+	//查看游戏状态
+	g3.Status()
 
 	//OutPut:
 	//Current HP:0, MP:0
 	//Current HP:5, MP:5
 	//Current HP:0, MP:0
 	//Current HP:5, MP:5
-
+	//Current HP:5, MP:5
 }
