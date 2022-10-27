d8ef94d65d260bd4196fdcdee9cc3e6d96808f4c
simbahebinbo <lansheng228@protonmail.com>
2022-10-27 19:00:21 +0800

    编译通过

--- interperter/interperter_test.go
+++ interperter/interperter_test.go
@@ -1,8 +1,11 @@
 package interperter
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
-func ExampleTerminalExpression_Interpret() {
+func TestExampleTerminalExpression_Interpret(t *testing.T) {
 	isMale := &OrExpression{&TerminalExpression{"Robert"}, &TerminalExpression{"John"}}
 	fmt.Println("John is male?", isMale.Interpret("John"))
 	isMarriedWoman := &AndExpression{&TerminalExpression{"Julie"}, &TerminalExpression{"Married"}}
