efbbcb18da71ed89c6b54ee697d87150db3533fc
simbahebinbo <lansheng228@protonmail.com>
2022-10-27 15:11:58 +0800

    编译通过

--- iterator/iterator_test.go
+++ iterator/iterator_test.go
@@ -1,6 +1,8 @@
 package iterator
 
-func ExamplePeoplesIterator_Iterator() {
+import "testing"
+
+func TestExamplePeoplesIterator_Iterator(t *testing.T) {
 	p1 := &Person{"小明01", 16, "足球"}
 	p2 := &Person{"小明02", 17, "篮球，足球"}
 	ps := &Peoples{}
@@ -10,5 +12,4 @@ func ExamplePeoplesIterator_Iterator() {
 	//OutPut:
 	//姓名:小明01,年龄:16,爱好:足球
 	//姓名:小明02,年龄:17,爱好:篮球，足球
-
 }
